f3d76c87ac22f7b0dec4c7f24962b8819d2d3e1b
danir28 <d.r7428331@gmail.com>
2026-05-20 22:13:49 -0300

    Agrego carrito dinamico
    Se implementa carrito con renderizado dinamico y control de cantidades

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.getElementById("productos-container");
+const carritoContainer = document.getElementById("carrito-container");
+let carrito = [];
 
 productos.forEach(producto => {
   const card = document.createElement("div");
@@ -8,10 +10,50 @@ productos.forEach(producto => {
     <img src="${producto.imagen}" alt="${producto.nombre}">
     <h3>${producto.nombre}</h3>
     <p>${producto.descripcion}</p>
-    <a href="https://wa.me/5493493436619?text=Hola,%20quiero%20pedir%20${producto.nombre}" class="btn">
-  Pedir por WhatsApp
-</a>
+    <button class="btn agregar-btn" data-id="${producto.id}">
+  Agregar al carrito
+</button>
   `;
 
   container.appendChild(card);
+
 });
+
+const botonesAgregar = document.querySelectorAll(".agregar-btn");
+  botonesAgregar.forEach(boton => {
+    boton.addEventListener("click", () => {
+      const id = Number(boton.getAttribute("data-id"));
+      agregarAlCarrito(id);
+    });
+  })
+
+function agregarAlCarrito(id) {
+  const productoEncontrado = productos.find(
+    producto => producto.id === id
+  );
+  const productoEnCarrito = carrito.find(
+    producto => producto.id === id
+  );
+  if (productoEnCarrito) {
+    productoEnCarrito.cantidad++;
+  } else {
+    carrito.push({
+      ...productoEncontrado,
+      cantidad: 1
+    });
+  }
+  console.log(carrito);
+  renderizarCarrito();
+}
+
+function renderizarCarrito() {
+  carritoContainer.innerHTML = "";
+  carrito.forEach(producto => {
+    carritoContainer.innerHTML += `
+      <div class="item-carrito">
+        <h3>${producto.nombre}</h3>
+        <p>Cantidad: ${producto.cantidad}</p>
+      </div>
+    `;
+  })
+}
